561ceeda2bf9552fc6edd49bea64fe51a8968484
Atr1ka <b.johnson5714@outlook.com>
2022-02-27 17:23:19 +0000

    yes

--- client.js
+++ client.js
@@ -2,21 +2,52 @@ function main() {
     submit(JSON.stringify(question));
 }
 
-function type(nameVal, text) {
-    $('input[name=' + nameVal + ']').val(text);
+function type(type, text, pos, sign='=') {
+    if (typeof mathField === 'undefined') var mathField;
+    switch (type) {
+        case 'input':
+            $('input[name=' + pos + ']').val(text);
+            break;
+        case 'exp': 
+            mathField = MathQuill.MathField($('.eqnsolutions-answerinput')[pos]);
+            try {
+                mathField.latex("");
+            }
+            catch(e) {
+                mathField = MathQuill.MathField($('#expression-answer')[pos]);
+                mathField.latex("")
+            }
+            mathField.write(text);
+            if (sign !== '=') {
+                $(".solution-rows").find($("select"))[pos].value = sign;
+            }
+            break;
+        case 'coords':
+            mathField = MathQuill.MathField($('.expression-answer-' + pos)[0]);
+            mathField.latex("");
+            mathField.write(text);
+            break;
+        case 'frac':
+            mathField = MathQuill.MathField($('#expression-answer')[0]);
+            mathField.latex("");
+            mathField.write(text);
+            break;
+        case 'mcq':
+            $('input:radio[name="multiplechoice-answer[]"][value="' + text + '"]').attr('checked',true);
+            break;
+        case 'vector': 
+            mathField = MathQuill.MathField($('.expression-answer-cell')[pos]);
+            mathField.latex("");
+            mathField.write(text);
+            break;
+    }
 }
 
-function typeMQ(text) {
-    let mathField = MathQuill.MathField($('#expression-answer')[0]);
-    mathField.write(text);
+function clearMQC() {
+    for (let i=0;i<$('input:radio[name="multiplechoice-answer[]"]').length;++i)
+        $('input:radio[name="multiplechoice-answer[]"][value="' + i + '"]').attr('checked',false);
 }
 
-function typeCoords(text) {
-    MathQuill = MathQuill.getInterface(1);
-    let mathField = MathQuill.MathField($('.expression-answer-x mq-editable-field mq-math-mode')[0]);
-    mathField.write(text);
-}
-// expression-answer-x mq-editable-field mq-math-mode
 function cleanJSON(og) {
     let out = "";
     for (i=0;i<og.length;++i) {
@@ -45,9 +76,13 @@ function submit(str) {
             if (data.typeCount == 0)
                 alert(data.alertString);
             else {                   
-                //MathQuill = MathQuill.getInterface(1);
+                if (typeof MathQuill.getInterface == 'function') {
+                    MathQuill = MathQuill.getInterface(1);
+                }
+                if (question.answer.type === 'multiplechoice')
+                    clearMQC();
                 for (i=0;i<data.typeCount;++i) {
-                    type(data.toType[i].nameVal, data.toType[i].text);
+                    type(data.toType[i].type, data.toType[i].text, data.toType[i].pos, data.toType[i].sign);
                 }
             }
         },
